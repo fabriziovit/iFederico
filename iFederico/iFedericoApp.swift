b5a3f6bb27f3720f403d4d7d703efb1c7c2af4ea
Fabrizio Vitale <fabri_1999@hotmail.it>
2022-12-07 12:34:38 +0100

    Initial Commit

---
+++ iFederico/iFedericoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFedericoApp.swift
+//  iFederico
+//
+//  Created by Fabrizio Vitale on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct iFedericoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
